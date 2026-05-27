3cb0c2beca23c1dabbdd3caf749234b6a2fd681c
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-05-27 03:42:54 +0200

    fix: #216fd9bc3433717b1b4967aa534d87103f391341

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -7,7 +7,6 @@ return {
   config = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- Neovim 0.11+: vim.lsp.config en lugar de lspconfig
     vim.lsp.config("lua_ls", {
       capabilities = capabilities,
       settings = {
@@ -18,8 +17,17 @@ return {
         },
       },
     })
+
     vim.lsp.config("nixd", { capabilities = capabilities })
-    vim.lsp.config("csharp_ls", { capabilities = capabilities })
+
+    vim.lsp.config("csharp_ls", {
+      capabilities = capabilities,
+      -- Solo arranca con ficheros C# o en directorios con proyecto .NET
+      root_dir = function(fname)
+        local util = require("lspconfig.util")
+        return util.root_pattern("*.sln", "*.csproj", "*.cs")(fname)
+      end,
+    })
 
     vim.lsp.enable({ "lua_ls", "nixd", "csharp_ls" })
 
