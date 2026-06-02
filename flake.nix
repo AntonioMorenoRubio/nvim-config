eced91b098ffc1e8e5f47b180886552be992ce2c
Antonio Moreno Rubio <amrinformatica10@gmail.com>
2026-06-02 22:10:58 +0200

    updated to 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Antonio's Neovim configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
   };
 
   outputs = { nixpkgs, ... }: {
